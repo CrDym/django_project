c0ff258a3739c7cf972368800b38c76534dad84e
cr <739965647@qq.com>
2020-09-16 23:29:11 +0800

    feat:支持了markdown语法

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ArticlePost
+import markdown
 
 
 def article_list(request):
@@ -10,11 +11,22 @@ def article_list(request):
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
 
+
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         # 包含缩写、表格等常用扩展
+                                         'markdown.extensions.extra',
+                                         # 语法高亮扩展
+                                         'markdown.extensions.codehilite',
+                                     ])
+
     # 需要传递给模板的对象
     context = {'article': article}
-    # 载入模板，返回contex对象
+    # 载入模板，返回context对象
     return render(request, 'article/detail.html', context)
 
