b9a6c9858736df84baca8cad46eb04535d799cb7
Rock <739965647@qq.com>
2020-09-16 16:34:49 +0800

    feat:创建了model模型

---
+++ my_blog/article/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/16 16:00
+# @Author  : Rock
+# @File    : urls.py
+# @describe:
+
+from django.urls import path
+
+app_name = 'article'
+
+urlpatterns = [
+
+]
