52e6c4def867fc470b7dc6505398770bb0c29ccd
cr <739965647@qq.com>
2020-09-16 23:03:37 +0800

    feat:新增了文章详情界面与文章列表跳转

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -9,3 +9,12 @@ def article_list(request):
     context = {'articles': articles}
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = {'article': article}
+    # 载入模板，返回contex对象
+    return render(request, 'article/detail.html', context)
+
